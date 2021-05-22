80a24c44fbfd2a00826299816dcef827d1e82fef
Jovan Nixon <jmnfire@gmail.com>
2021-05-22 01:15:57 -0600

    more errors

--- models/Comment.js
+++ models/Comment.js
@@ -1,67 +1,67 @@
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
-// class Comment extends Model {}
-// Comment.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     comment_description: {
-//       type: DataTypes.STRING,
-//     },
-//     date_created: {
-//       type: DataTypes.DATE,
-//       allowNull: false,
-//       defaultValue: DataTypes.NOW,
-//     },
-//     blog_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'blog',
-//         key: 'id',
-//       },
-//     },
-//     user_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: 'user',
-//         key: 'id',
-//       },
-//     },
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'comment',
-//   }
-// );
-// module.exports = Comment;
-
-
-
-
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
 class Comment extends Model {}
-
 Comment.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     comment_description: {
-            type: DataTypes.STRING,
-          },
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
   },
   {
-    sequelize
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
   }
 );
-
 module.exports = Comment;
+
+
+
+
+// const { Sequelize, Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
+
+// class Comment extends Model {}
+
+// Comment.init(
+//   {
+//     comment_description: {
+//             type: DataTypes.STRING,
+//           },
+//   },
+//   {
+//     sequelize
+//   }
+// );
+
+// module.exports = Comment;
 
 
 
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,52 +1,53 @@
-// const commentFormHandler = async function(event) {
-//   event.preventDefault();
+const commentFormHandler = async function(event) {
+  event.preventDefault();
 
-//   // const postId = document.querySelector('input[name="post-id"]').value;
-//   const body = document.querySelector('#comment_description').value.trim();
+      const blog_id = document.querySelector('.comment-form').dataset.blogid;
+  const comment_description = document.querySelector('#comment_description').value.trim();
 
-//   if (body) {
-//     await fetch('/api/comments', {
-//       method: 'POST',
-//       body: JSON.stringify({
-//         // postId,
-//         body
-//       }),
-//       headers: {
-//         'Content-Type': 'application/json'
-//       }
-//     });
-
-//     document.location.reload();
-//   }
-// };
-
-// document
-//   .querySelector('#new-comment-form')
-//   .addEventListener('submit', commentFormHandler);
-
-
-
-
-
-const commentFormHandler = async (event) => {
-    event.preventDefault();
-    const body = document.querySelector('#comment_description').value.trim();
-    const blog_id = document.querySelector('.comment-form').dataset.blogid;
-    if (body) {
-      const response = await fetch('/api/comments', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({body}),
-        
-      });
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
+  if (comment_description) {
+    console.log("testing post for comments")
+    await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        blog_id,
+        comment_description
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
-    }
-  };
-  document
-  .querySelector('.comment-form')
+    });
+    console.log("post for comments compleat")
+    document.location.reload();
+  }
+};
+
+document
+  .querySelector('#new-comment-form')
   .addEventListener('submit', commentFormHandler);
+
+
+
+
+
+// const commentFormHandler = async (event) => {
+//     event.preventDefault();
+//     const body = document.querySelector('#comment_description').value.trim();
+//     const blog_id = document.querySelector('.comment-form').dataset.blogid;
+//     if (body) {
+//       const response = await fetch('/api/comments', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({body}),
+        
+//       });
+//       if (response.ok) {
+//         document.location.replace('/dashboard');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   };
+//   document
+//   .querySelector('.comment-form')
+//   .addEventListener('submit', commentFormHandler);
 
